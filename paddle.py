0f68607c8defdaa89a657f8d97abd79219121360
Chaitanya Jobanputra <60739703+Jobanputra02@users.noreply.github.com>
2022-05-18 11:26:31 +0530

    Add files via upload

---
+++ paddle.py
@@ -0,0 +1,20 @@
+from turtle import Turtle, Screen
+
+
+class Paddle(Turtle):
+
+    def __init__(self, coordinates):
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.shapesize(stretch_wid=5, stretch_len=1)
+        self.penup()
+        self.goto(coordinates)
+
+    def go_up(self):
+        self.goto(self.xcor(), self.ycor() + 20)
+
+    def go_down(self):
+        self.goto(self.xcor(), self.ycor() - 20)
+
+
